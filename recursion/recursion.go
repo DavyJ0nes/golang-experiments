3161fe0e40193b633ac4e686ace42d7023e48f60
DavyJ0nes <davy.jones@me.com>
2018-07-25 08:21:37 +0100

    Add more examples about recursion.

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -18,3 +18,31 @@ func shapeAreaRecursion(n int) int {
 		return ((n * 4) - 4) + shapeAreaRecursion(n-1)
 	}
 }
+
+// add all elements of a slice together using recursion
+func sumSlice(input []int) int {
+	var x, sum int
+
+	if len(input) == 0 {
+		return 0
+	}
+
+	x, input = input[0], input[1:]
+	sum = x + sumSlice(input)
+
+	return sum
+}
+
+// count elements in slice using recursion
+func sliceCount(input []string) int {
+	var count int
+
+	if len(input) == 0 {
+		return 0
+	}
+
+	input = input[1:]
+	count = sliceCount(input) + 1
+
+	return count
+}
--- recursion/recursion_test.go
+++ recursion/recursion_test.go
@@ -4,15 +4,15 @@ import "testing"
 
 func TestLoopingArea(t *testing.T) {
 	tests := []struct {
-    name string
+		name  string
 		input int
-		want int
+		want  int
 	}{
-		{"1",1,1},
-		{"2",2,5},
-		{"3",3,13},
-		{"4",4,25},
-		{"5",5,41},
+		{"1", 1, 1},
+		{"2", 2, 5},
+		{"3", 3, 13},
+		{"4", 4, 25},
+		{"5", 5, 41},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -25,20 +25,88 @@ func TestLoopingArea(t *testing.T) {
 
 func TestRecursiveArea(t *testing.T) {
 	tests := []struct {
-    name string
+		name  string
 		input int
-		want int
+		want  int
 	}{
-		{"1",1,1},
-		{"2",2,5},
-		{"3",3,13},
-		{"4",4,25},
-		{"5",5,41},
+		{"1", 1, 1},
+		{"2", 2, 5},
+		{"3", 3, 13},
+		{"4", 4, 25},
+		{"5", 5, 41},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := shapeAreaRecursion(tt.input); got != tt.want {
 				t.Errorf("want: %v, got: %v", tt.want, got)
+			}
+		})
+	}
+}
+
+func Test_sumSlice(t *testing.T) {
+	type args struct {
+		input []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"basic test",
+			args{input: []int{2, 4, 6}},
+			12,
+		},
+		{
+			"empty input test",
+			args{input: []int{}},
+			0,
+		},
+		{
+			"longer input test",
+			args{input: []int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
+			45,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sumSlice(tt.args.input); got != tt.want {
+				t.Errorf("sumSlice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sliceCount(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"basic test",
+			args{input: []string{"do this", "and that"}},
+			2,
+		},
+		{
+			"longer test",
+			args{input: []string{"a", "b", "c", "d", "e", "f", "g"}},
+			7,
+		},
+		{
+			"empty input test",
+			args{input: []string{}},
+			0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sliceCount(tt.args.input); got != tt.want {
+				t.Errorf("sliceCount() = %v, want %v", got, tt.want)
 			}
 		})
 	}
